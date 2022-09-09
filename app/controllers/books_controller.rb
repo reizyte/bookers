6ddee2f1b2c0fa30cc207a5477110af8484bb783
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-09 12:59:12 +0000

    index-new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def index
+    @books = Book.all
+    @book = Book.new(book_params)
   end
 
   def show
+
   end
 
   def edit
   end
+
+
 end
