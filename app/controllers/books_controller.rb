b3604dee2a730b6f59732a55020e644117461910
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-10 14:13:07 +0000

    9/10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
-  def new
-  end
+  
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+   　 redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -20,10 +23,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
-end
+  end
